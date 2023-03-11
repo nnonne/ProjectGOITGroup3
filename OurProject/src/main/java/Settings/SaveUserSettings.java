6f1fc0c42ecdf0c6dfd6a829d181692f1ac679bd
kholodovaMB <holodova.maryna@gmail.com>
2023-03-11 22:30:59 +0200

    add SaveUserSettings

---
+++ OurProject/src/main/java/Settings/SaveUserSettings.java
@@ -0,0 +1,24 @@
+package Settings;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+
+public class SaveUserSettings {
+    public void saveUser(SettingsUserDto user){
+
+        String nameFile = "user" + String.valueOf(user.id) + ".json";
+        File file = new File(nameFile);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String toJson = gson.toJson(user);
+        try (OutputStream fos = Files.newOutputStream(file.toPath())) {
+            fos.write(toJson.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
