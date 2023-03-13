0737af300eeb6ddbb921726a37dd5f55194db2b6
mccreed13 <a.lapushenko13@gmail.com>
2023-03-13 17:01:21 +0200

    Create BankService, updated Enums/Currency, renamed 'BankNames' to 'BankName'

---
+++ OurProject/src/main/java/BankService/CurrencyRateMonoResponseDto.java
@@ -0,0 +1,17 @@
+package BankService;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@ToString
+public class CurrencyRateMonoResponseDto {
+    private int currencyCodeA;
+    private int currencyCodeB;
+    private BigDecimal rateSell;
+    private BigDecimal rateBuy;
+}
