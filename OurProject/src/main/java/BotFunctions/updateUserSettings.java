bf36fcb1dd073ad5374da8f33b6d4c82bb1b5bda
roma <romanezmolodez@gmail.com>
2023-03-14 22:22:02 +0200

    update settings

---
+++ OurProject/src/main/java/BotFunctions/updateUserSettings.java
@@ -0,0 +1,55 @@
+package BotFunctions;
+
+import org.apache.http.ParseException;
+import org.json.JSONObject;
+import org.json.JSONWriter;
+import org.json.simple.parser.JSONParser;
+
+import java.io.*;
+import java.util.List;
+
+public class updateUserSettings {
+    private void updateUserSettings(String idUser, String fieldName, String value) throws IOException, ParseException, org.json.simple.parser.ParseException {
+
+        Object obj = new JSONParser().parse(new FileReader("settings.json"));
+        JSONObject jsonObject = (JSONObject) obj;
+        JSONObject userSettings = (JSONObject) jsonObject.get(idUser);
+
+        switch (fieldName) {
+            case "Bank":
+                userSettings.remove("Bank");
+                userSettings.put("Bank", value);
+                break;
+            case "Currency":
+                String oldValue = (String) userSettings.get("Currency");
+                userSettings.put("Currency", /*oldValue + ", " + value*/ List.of(oldValue, value));
+                break;
+            case "Decimal":
+                userSettings.remove("Decimal");
+                userSettings.put("Decimal", value);
+                break;
+            case "NotificationTime":
+                userSettings.remove("NotificationTime");
+                userSettings.put("NotificationTime", value);
+                break;
+            default:
+                throw new IllegalArgumentException("Невірна назва поля: " + fieldName);
+        }
+
+        try (FileWriter file = new FileWriter("user" + idUser + ".json")) {
+            JSONWriter jsonWriter = new JSONWriter(file);
+            jsonWriter.object();
+            for (Object key : jsonObject.keySet()) {
+                String userIdKey = (String) key;
+                jsonWriter.key(userIdKey);
+                if (userIdKey.equals(idUser)) {
+                    jsonWriter.value(userSettings);
+                } else {
+                    jsonWriter.value(jsonObject.get(userIdKey));
+                }
+            }
+            jsonWriter.endObject();
+        }
+    }
+
+}
